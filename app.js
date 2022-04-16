cf3c892c4b23644f897369a2cb2f76b2b780c07a
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 13:00:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Area');
+
+alert('Hola, Bienvenide!');
+
+const base = prompt('Ingrese la base del triangulo');
+const altura = prompt('Ingrese la altura del triangulo');
+const area = (base*altura)/2;
+
+alert(`El area del triangulo es ${area}`);
